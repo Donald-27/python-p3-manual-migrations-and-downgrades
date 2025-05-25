57eb7f5393572c8cb5ee79f2f9bb25dc63d50f35
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-25 13:01:52 +0300

    implement manual migrations with Alembic

--- lib/models.py
+++ lib/models.py
@@ -1,32 +1,21 @@
-import os
-import sys
-
-sys.path.append(os.getcwd())
-
-
+#!/usr/bin/env python3
 from datetime import datetime
-
-from sqlalchemy import create_engine, desc
-from sqlalchemy import (CheckConstraint, UniqueConstraint,
-    Column, DateTime, Integer, String)
-
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, CheckConstraint, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///migrations_test.db')
-
 Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
-
+    
     id = Column(Integer(), primary_key=True)
-    name = Column(String(), index=True)
+    full_name = Column(String(), index=True)  
     email = Column(String(55))
     grade = Column(Integer())
     birthday = Column(DateTime())
     enrolled_date = Column(DateTime(), default=datetime.now())
+    updated_at = Column(DateTime(), default=datetime.now(), onupdate=datetime.now())
 
     def __repr__(self):
-        return f"Student {self.id}: " \
-            + f"{self.name}, " \
-            + f"Grade {self.grade}"
+        return f"Student {self.id}: {self.full_name}, Grade {self.grade}"
